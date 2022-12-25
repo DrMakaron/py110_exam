4e22c8e438078530f5c0d98b4ece6aee62e5a67c
andrei <andrew.skirchenko@gmail.com>
2022-12-25 13:42:41 +0300

    added: config.ini file added: Config class

---
+++ conf.py
@@ -0,0 +1,20 @@
+from configparser import ConfigParser
+
+
+class Config:
+    def __init__(self, config_file_path):
+        config = ConfigParser(allow_no_value=True)
+        config.read(config_file_path)
+        sections = config.sections()
+
+        for section in sections:
+            for key, value in config.items(section):
+                if value:
+                    setattr(self, key.upper(), str(value))
+                else:
+                    pass
+
+
+if __name__ == '__main__':
+    cfg = Config('config.ini')
+    print(dir(cfg), cfg.MODEL)
---
+++ main.py
@@ -0,0 +1,8 @@
+from conf import Config
+
+
+CFG_FILE = 'config.ini'
+
+
+if __name__ == '__main__':
+    config = Config(CFG_FILE)
